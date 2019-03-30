1c0a579b0f44f4d28e856e5f53c7c6a2e676be67
miraSantos <miraflor.santos@nyu.edu>
2019-03-30 12:02:30 +0400

    updated things

--- phyloseq_analysis.R
+++ phyloseq_analysis.R
@@ -2,3 +2,25 @@ if(!requireNamespace("BiocManager")){
   install.packages("BiocManager")
 }
 BiocManager::install("phyloseq")
+
+library(phyloseq); packageVersion("phyloseq")
+library(ggplot2); packageVersion("ggplot2")
+
+theme_set(theme_bw())
+
+samples.out <- rownames(seqtab.nochim)
+subject <- sapply(strsplit(samples.out, "D"), `[`, 1)
+gender <- substr(subject,1,1)
+subject <- substr(subject,2,999)
+day <- as.integer(sapply(strsplit(samples.out, "D"), `[`, 2))
+samdf <- data.frame(Subject=subject, Gender=gender, Day=day)
+samdf$When <- "Early"
+samdf$When[samdf$Day>100] <- "Late"
+rownames(samdf) <- samples.out
+
+
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
+ps <- prune_samples(sample_names(ps) != "Mock", ps) # Remove mock sample
+ps
--- single_pass for Bacteria.R
+++ single_pass for Bacteria.R
@@ -1,21 +1,12 @@
-source("http://bioconductor.org/biocLite.R")
-biocLite(suppressUpdates = FALSE)
-biocLite("ShortRead", suppressUpdates = FALSE)
-
-biocLite("devtools")
-library("devtools")
-devtools::install_github("benjjneb/dada2") # use the ref="v1.10" (e.g.) argument to get specific versions
-
-
-library(dada2);packageVersion("dada2")
-path <- "C:/Users/mps48/Documents/1111AAA_2019_Spring/Capstone/Sequencing/sequencing_data/fastq_files/Euk_1391f_EukBr"
+library(dada2) #packageVersion("dada2")
+path <- "C:/Users/mps48/Documents/1111AAA_2019_Spring/Capstone/Sequencing/sequencing_data/fastq_files/V4_515F_New_V4_806R_New"
 
 
 # Forward and reverse fastq filenames have format: SAMPLENAME_R1_001.fastq and SAMPLENAME_R2_001.fastq
 fnFs <- sort(list.files(path, pattern="_R1.fastq", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern="_R2.fastq", full.names = TRUE))
 # Extract sample names, assuming filenames have format: SAMPLENAME_XXX.fastq
-sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`,3)
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`,6)
 
 
 # Plotting Quality Proflies -----------------------------------------------
@@ -46,21 +37,19 @@ dir.create("C:/Users/mps48/Documents/1111AAA_2019_Spring/Capstone/Sequencing/seq
 #-a is for 3' trimming
 #-g is for 5' trimming
 for (ii in 1:length(fnFs)){
-  text <- paste("cutadapt -a GTAGGTGAACCTGCAGAAGGATCA -g GTACACACCGCCCGTC -q 2,2 -e 0.2 -l 150 -o ", filtFs_CA[ii]," ",fnFs[ii],sep="")
+  text <- paste("cutadapt -g GTGYCAGCMGCCGCGGTAA -a ATTAGAWACCCBNGTAGTCC  -q 2,2 -e 0.2 -l 150 -o ", filtFs_CA[ii]," ",fnFs[ii],sep="")
   system(text)
   print(paste(ii ," of ", length(fnFs)))
 }
 
 for (ii in 1:length(fnRs)){
-  text <- paste("cutadapt -a GACGGGCGGTGTGTAC  -g TGATCCTTCTGCAGGTTCACCTAC -q 2,2 -e0.2 -l 150  -o ", filtRs_CA[ii]," ",fnRs[ii],sep="")
+  text <- paste("cutadapt -g GGACTACNVGGGTWTCTAAT -a TTACCGCGGCKGCTGRCAC  -q 2,2 -e0.2 -l 150  -o ", filtRs_CA[ii]," ",fnRs[ii],sep="")
   system(text)
   print(paste(ii ," of ", length(fnRs)))
 }
 
 # I go through dada2's filter because when I just fed the cutadapted files R crashed.
 #notsure why this is happening
-#i rerouted through dada2's filter. Sthing is happening that I am not understanding
-#why can't i just go through filter and trims device.
 out <- filterAndTrim(filtFs_CA, filtFs, filtRs_CA, filtRs, rm.phix=FALSE,
                      compress=TRUE, multithread=FALSE) # On Windows set multithread=FALSE
 head(out)
@@ -73,9 +62,9 @@ errR <- learnErrors(filtRs, multithread=FALSE) #okay I tried it with multithread
 plotErrors(errF, nominalQ=TRUE)
 plotErrors(errR, nominalQ=TRUE)
 
-#Dereplication -----------------------------------------------------------
-
-derepFs <- derepFastq(filtFs, verbose=TRUE)
+Dereplication -----------------------------------------------------------
+  
+  derepFs <- derepFastq(filtFs, verbose=TRUE)
 derepRs <- derepFastq(filtRs, verbose=TRUE)
 # Name the derep-class objects by the sample names
 names(derepFs) <- sample.names
@@ -87,7 +76,6 @@ names(derepRs) <- sample.names
 dadaFs <- dada(derepFs, err=errF, multithread=FALSE)
 dadaRs <- dada(derepRs, err=errR, multithread=FALSE)
 dadaFs[[1]]
-dadaRs[[1]]
 
 
 # Merge Paired Reads ------------------------------------------------------
@@ -104,7 +92,7 @@ dim(seqtab)
 
 # Remove Chimeras ---------------------------------------------------------
 
-seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=FALSE, verbose=TRUE)
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
 dim(seqtab.nochim)
 
 sum(seqtab.nochim)/sum(seqtab)
@@ -123,35 +111,13 @@ head(track)
 
 
 # Assign Taxonomy ---------------------------------------------------------
-set.seed(100)
 
-setwd("C:/Users/mps48/Documents/1111AAA_2019_Spring/Capstone/Sequencing/ref_databases/")
-taxa <- assignTaxonomy(seqtab.nochim, "silva_nr_v132_train_set.fa.gz", multithread=TRUE)
-taxa <- assignTaxonomy(seqtab.nochim, "gg_13_8_train_set_97.fa.gz", multithread=FALSE)
-taxa <- assignTaxonomy(seqtab.nochim, "rdp_train_set_16.fa.gz", multithread=FALSE)
-taxa <- addSpecies(taxa, "C:/Users/mps48/Documents/1111AAA_2019_Spring/Capstone/Sequencing/silva_species_assignment_v128.fa")
-
+taxa <- assignTaxonomy(seqtab.nochim, "~/tax/silva_nr_v128_train_set.fa.gz", multithread=TRUE)
+taxa <- addSpecies(taxa, "~/tax/silva_species_assignment_v128.fa.gz")
 
 
 
 # Alternative Way w/ Decipher ---------------------------------------------
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("DECIPHER", version = "3.8")
-
 library(DECIPHER); packageVersion("DECIPHER")
-
-dna <- DNAStringSet(getSequences(seqtab.nochim)) # Create a DNAStringSet from the ASVs
-load("~/tax/IDTaxa/SILVA_SSU_r132_March2018.RData") # CHANGE TO THE PATH OF YOUR TRAINING SET
-ids <- IdTaxa(dna, trainingSet, strand="top", processors=NULL, verbose=FALSE) # use all processors
-ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") # ranks of interest
-# Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
-taxid <- t(sapply(ids, function(x) {
-  m <- match(ranks, x$rank)
-  taxa <- x$taxon[m]
-  taxa[startsWith(taxa, "unclassified_")] <- NA
-  taxa
-}))
-colnames(taxid) <- ranks; rownames(taxid) <- getSequences(seqtab.nochim)
 
